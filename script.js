0f2e729cfb655188bde043f1b9ac48074a08fbb3
Najad <mrnajadas@gmail.com>
2025-12-18 12:17:34 +0530

    ui is updated to everything in real time

--- script.js
+++ script.js
@@ -18,10 +18,10 @@ function getComputerChoice(){
 }
 
 //taking input from human using prompt 
-function getHumanChoice(){
-    const choice = prompt("Rock, Paper or Scissors?");
-    return choice.toLowerCase();
-}
+// function getHumanChoice(){
+//     const choice = prompt("Rock, Paper or Scissors?");
+//     return choice.toLowerCase();
+// }
 
 //assigning choices to variables
 
@@ -105,18 +105,86 @@ function playRound(humanSelection,computerSelection){
 
 
 //Logic to play the round 5 times
-for (let i = 0; i < 5 ; i++){
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-}
+// for (let i = 0; i < 5 ; i++){
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+// }
 
 
 //FINAL RESULT 
-if (humanScore > computerScore) {
-    console.log(`You win the game! Final score: You ${humanScore} - ${computerScore} Computer`);
-} else if (computerScore > humanScore) {
-    console.log(`Computer wins the game! Final score: Computer ${computerScore} - ${humanScore} You`);
-} else {
-    console.log(`The game is a tie! Final score: ${humanScore} - ${computerScore}`);
+
+
+
+//event listener for buttons rock paper and scissor  that call your playRound
+//  function with the correct playerSelection every time a button is clicked
+
+
+
+
+const rock = document.getElementById("Rock");
+const paper = document.getElementById("Paper");
+const scissor = document.getElementById("Scissor");
+
+rock.addEventListener("click",()=>{
+    const computer= getComputerChoice()
+    playRound("rock",computer)
+    resultPara = document.getElementById("choices-result")
+    resultPara.innerHTML = `
+    <strong>Your choice:</strong> Rock<br>
+    <strong>Computer choice:</strong> ${computer}<br><br>
+    <strong>Your score:</strong> ${humanScore} &nbsp;
+    <strong>Computer score:</strong> ${computerScore}
+    `;
+    checkWinner()
+})
+
+paper.addEventListener("click",()=>{
+    const computer= getComputerChoice()
+    playRound("paper",computer)
+    resultPara = document.getElementById("choices-result")
+    resultPara.innerHTML = `
+    <strong>Your choice:</strong> Paper<br>
+    <strong>Computer choice:</strong> ${computer}<br><br>
+    <strong>Your score:</strong> ${humanScore} &nbsp;
+    <strong>Computer score:</strong> ${computerScore}
+    `;
+    checkWinner()
+})
+scissor.addEventListener("click",()=>{
+    const computer= getComputerChoice()
+    playRound("scissors",computer)
+    resultPara = document.getElementById("choices-result")
+    resultPara.innerHTML = `
+    <strong>Your choice:</strong> Scissors<br>
+    <strong>Computer choice:</strong> ${computer}<br><br>
+    <strong>Your score:</strong> ${humanScore} &nbsp;
+    <strong>Computer score:</strong> ${computerScore}
+    `;
+    checkWinner()
+})
+
+function checkWinner(){
+    if (humanScore===5){
+        finalResult = document.body.querySelector(".finalResult")
+        finalResult.innerHTML = `<strong> You win bro`
+        rock.disabled=true;
+        paper.disabled=true;
+        scissor.disabled=true;
+
+    }
+    else if(computerScore===5){
+        finalResult = document.querySelector(".finalResult")
+        finalResult.innerHTML = `<strong> Computer win bro,You lost,its okay,You can try again`
+        rock.disabled=true;
+        paper.disabled=true;
+        scissor.disabled=true;
+    }
 }
+// if (humanScore > computerScore) {
+//     console.log(`You win the game! Final score: You ${humanScore} - ${computerScore} Computer`);
+// } else if (computerScore > humanScore) {
+//     console.log(`Computer wins the game! Final score: Computer ${computerScore} - ${humanScore} You`);
+// } else {
+//     console.log(`The game is a tie! Final score: ${humanScore} - ${computerScore}`);
+// }
